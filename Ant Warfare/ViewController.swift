b7c563eb1f619bc629b97c57bc7c59707d158b4c
Daniel Eke <endanke@gmail.com>
2014-09-29 09:57:22 +0200

    Initial Commit

---
+++ Ant Warfare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ant Warfare
+//
+//  Created by Daniel Eke on 2014. 09. 29..
+//  Copyright (c) 2014. endanke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
